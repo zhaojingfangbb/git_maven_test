a9b05e57a8fdab9f9c1a623fd914f2a6f9db6c66
zhaojingfang <zjf15136545711@163.com>
2020-09-11 11:19:37 +0800

    创建helloworld类，输出helloworld

---
+++ src/main/java/com/gitMavenTest/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.gitMavenTest;
+
+/**
+ * @program: gitMavenTest
+ * @description:
+ * @author: zz
+ * @create: 2020-09-11 11:18:13
+ **/
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+
+}
