8646623f279d66abd101a8a1a77e8be7514dfbb3
zhaojingfang <zjf15136545711@163.com>
2020-09-11 11:58:11 +0800

    增加了一句输出helloworld

--- src/main/java/com/gitMavenTest/HelloWorld.java
+++ src/main/java/com/gitMavenTest/HelloWorld.java
@@ -9,10 +9,11 @@ package com.gitMavenTest;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello world");
-System.out.println("hello world");
-System.out.println("hello world");
-System.out.println("hello world");
-System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
     }
 
 }
