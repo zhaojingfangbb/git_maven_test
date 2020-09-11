45b2e09fd38c8fbfb40f1e391adf46738cb4b1e4
zhaojingfangbb <71064149+zhaojingfangbb@users.noreply.github.com>
2020-09-11 11:42:02 +0800

    Update HelloWorld.java

--- src/main/java/com/gitMavenTest/HelloWorld.java
+++ src/main/java/com/gitMavenTest/HelloWorld.java
@@ -9,6 +9,7 @@ package com.gitMavenTest;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello world");
+System.out.println("hello world");
     }
 
 }
